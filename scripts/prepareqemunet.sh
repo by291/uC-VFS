53175090f9cfbda1f71a824aa70bc384d6334278
by291 <bystander291@gmail.com>
2025-05-02 15:03:58 +0800

    refactor: cleanup for uC-VFS

--- scripts/prepareqemunet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Do not run this script as root
-
-sudo iptables -t nat -A POSTROUTING -j MASQUERADE -s 192.0.2.3
-sudo sysctl -w net.ipv4.ip_forward=1
-
-sudo ./scripts/net-setup.sh
